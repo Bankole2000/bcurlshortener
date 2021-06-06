a02e126c288545d5535182e33ce738683330dd66
Bankole Esan <techybanky@gmail.com>
2021-06-06 15:12:37 +0100

    minor edits

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ], 
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Shrtnr | URl Shortener";
+      return args;
+    });
+  },  
   publicPath: process.env.NODE_ENV === "production" ? "/bcurlshortener/" : "/"
 }
